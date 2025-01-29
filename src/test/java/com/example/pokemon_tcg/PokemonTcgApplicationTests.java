aa6e50cc62a58ed28607cfd805819a9f1495f7a2
fluffykoo <oumou.camara@efrei.net>
2025-01-29 14:05:27 +0100

    first commit

---
+++ src/test/java/com/example/pokemon_tcg/PokemonTcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_tcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
