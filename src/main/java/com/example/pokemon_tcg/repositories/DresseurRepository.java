1f29ff4397aa0fc221343466f01b5d44cd5cfaa3
Adrrien04 <adrrien.chandrakumar@gmail.com>
2025-01-29 15:39:53 +0100

    DresseurRepository

--- src/main/java/com/example/pokemon_tcg/repositories/DresseurRepository.java
+++ src/main/java/com/example/pokemon_tcg/repositories/DresseurRepository.java
@@ -1,4 +1,9 @@
 package com.example.pokemon_tcg.repositories;
 
-public interface DresseurRepository {
+import com.example.pokemon_tcg.models.Dresseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DresseurRepository extends JpaRepository<Dresseur, String> {
 }
