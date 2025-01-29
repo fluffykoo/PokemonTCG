2c88a829a7e8ba3ba6e3c69442b50c08957ce025
Adrrien04 <adrrien.chandrakumar@gmail.com>
2025-01-29 14:20:46 +0100

    test

--- src/main/java/com/example/pokemon_tcg/controllers/HomeController.java
+++ src/main/java/com/example/pokemon_tcg/controllers/HomeController.java
@@ -1,4 +1,18 @@
 package com.example.pokemon_tcg.controllers;
 
-public class HomeController {
-}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    @RequestMapping("/home")
+    public class HomeController {
+
+        @GetMapping("/{prenom}")
+        public String greetingName(@PathVariable String prenom) {
+            return "Salut " + prenom;
+        }
+
+    }
+
