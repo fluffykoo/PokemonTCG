fbc54e7a10eec27e5487ca4f2cafe27623fad00a
Adrrien04 <adrrien.chandrakumar@gmail.com>
2025-01-29 15:40:48 +0100

    PokemonRepository

--- src/main/java/com/example/pokemon_tcg/repositories/PokemonRepository.java
+++ src/main/java/com/example/pokemon_tcg/repositories/PokemonRepository.java
@@ -1,4 +1,12 @@
 package com.example.pokemon_tcg.repositories;
 
-public interface PokemonRepository {
+import com.example.pokemon_tcg.constants.TypePokemon;
+import com.example.pokemon_tcg.models.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PokemonRepository extends JpaRepository<Pokemon, String> {
+    List<Pokemon> findAllByType(TypePokemon type);
+
 }
